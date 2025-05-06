b2deaf65707ac44df6906ea9181dcb5b1fb3b3be
JoaoUgulini <joao.2023001218@aluno.iffar.edu.br>
2025-05-06 17:11:21 -0300

    Cadastro de Imoveis Concluida

---
+++ backend/CadastrarImovel.php
@@ -0,0 +1,78 @@
+<?php
+include_once 'conecao.php';
+session_start();
+if (isset($_FILES['imgImovel'])) {
+    $arquivo = $_FILES['imgImovel'];
+
+    if ($arquivo['error']) {
+        echo "Erro ao enviar o arquivo";
+        exit;
+    }
+    $pasta = "../imgImovel/";
+    $nomeArquivo = $arquivo['name'];
+    $novoArqNome = uniqid();
+    $extensao = strtolower(pathinfo($nomeArquivo, PATHINFO_EXTENSION));
+    if ($extensao != "jpg" && $extensao != "png" && $extensao != "jpeg") {
+        die("Tipo de arquivo nao aceito");
+    }
+    $caminhoCompleto = $pasta . $novoArqNome . "." . $extensao;
+    if (move_uploaded_file($arquivo['tmp_name'], $caminhoCompleto)) {
+        $titulo = $_POST['nomeImovel'];
+        $cpf = $_POST['cpf'];
+        $tipo = $_POST['tipoImovel'];
+        $finalidade = $_POST['finalidade'];
+        $valor = $_POST['valor'];
+        $medida_frente = $_POST['medidaFrente'];
+        $medida_lateral = $_POST['medidaLateral'];
+        $quartos = $_POST['quarto'];
+        $banheiros = $_POST['banheiro'];
+        $vagas_garagem = $_POST['garagem'];
+        $endereco = $_POST['endereco'];
+        $numero = $_POST['numero'];
+        $complemento = $_POST['complemento'];
+        $bairro = $_POST['bairro'];
+        $cidade = $_POST['cidade'];
+        $estado = $_POST['estado'];
+        $path = $caminhoCompleto;
+
+        $conexao = conectar("bdimovel");
+        $sqlID = "SELECT id FROM cliente WHERE cpf = :cpf";
+        $stmt = $conexao->prepare($sqlID);
+        $stmt->bindValue(':cpf', $cpf);
+        $stmt->execute();
+        $resultado = $stmt->fetch(PDO::FETCH_ASSOC);
+        if ($resultado) {
+            $id_Proprietario = $resultado['id'];
+        } else {
+            die("CPF não encontrado no cadastro de clientes");
+        }
+        $sql = "Insert into imovel (id_Proprietario, titulo, tipo, finalidade, valor, medida_frente, medida_lateral, quartos, banheiros, vagas_garagem, endereco, numero, complemento, bairro, cidade, estado, path) values (:id_Proprietario, :titulo, :tipo, :finalidade, :valor, :medida_frente, :medida_lateral, :quartos, :banheiros, :vagas_garagem, :endereco, :numero, :complemento, :bairro, :cidade, :estado, :path)";
+        $pstmt = $conexao->prepare($sql);
+
+        $pstmt->bindValue(':id_Proprietario', $id_Proprietario);
+        $pstmt->bindValue(':titulo', $titulo);
+        $pstmt->bindValue(':tipo', $tipo);
+        $pstmt->bindValue(':finalidade', $finalidade);
+        $pstmt->bindValue(':valor', $valor);
+        $pstmt->bindValue(':medida_frente', $medida_frente);
+        $pstmt->bindValue(':medida_lateral', $medida_lateral);
+        $pstmt->bindValue(':quartos', $quartos);
+        $pstmt->bindValue(':banheiros', $banheiros);
+        $pstmt->bindValue(':vagas_garagem', $vagas_garagem);
+        $pstmt->bindValue(':endereco', $endereco);
+        $pstmt->bindValue(':numero', $numero);
+        $pstmt->bindValue(':complemento', $complemento);
+        $pstmt->bindValue(':bairro', $bairro);
+        $pstmt->bindValue(':cidade', $cidade);
+        $pstmt->bindValue(':estado', $estado);
+        $pstmt->bindValue(':path', $path);
+        $pstmt->execute();
+        $conexao = encerrar();
+        echo "Imovel cadastrado";
+        header("Location: ../frontend-sistema/area_funcionario.php");
+        exit;
+    }
+}
+?>
+
+?>
---
+++ frontend-sistema/TelaCadImovel.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Tela de Cadastro de Imovel</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;700&display=swap" rel="stylesheet">
+    <link href="../CSS/styleSistema.css" rel="stylesheet">
+</head>
+<body>
+<form enctype="multipart/form-data" action="../backend/CadastrarImovel.php" method="post">
+    <label>Nome do Imovel</label>
+    <input type="text" name="nomeImovel" placeholder="Nome do Imovel">
+    <label>CPF do Dono</label>
+    <input type="text" name="cpf" placeholder="CPF do Dono"
+    <label>Tipo do Imovel</label>
+    <select name="tipoImovel">
+        <option value="casa">Casa</option>
+        <option value="apartamento">Apartamento</option>
+        <option value="kitnet">Kitnet</option>
+        <option value="terreno">Terreno</option>
+        <option value="comercial">Espaço Comercial</option>
+        <option value="fazenda">Fazenda</option>
+    </select>
+    <label>Finalidade</label>
+    <select name="finalidade">
+        <option value="venda">Venda</option>
+        <option value="aluguel">Aluguel</option>
+    </select>
+    <label>Valor</label>
+    <input type="text" name="valor" placeholder="Valor"><br>
+    <label>Medidas</label>
+    <div class="medidas-group">
+        <input type="text" name="medidaFrente" placeholder="Medida da Frente">
+        <input type="text" name="medidaLateral" placeholder="Medida da Lateral">
+    </div>
+    <label>Detalhes</label>
+    <div class="detalhes-group">
+        <input type="text" name="quarto" placeholder="Quartos">
+        <input type="text" name="banheiro" placeholder="Banheiros">
+        <input type="text" name="garagem" placeholder="Garagem">
+    </div>
+    <label>Endereço</label>
+    <div class="endereco-group">
+        <input type="text" name="endereco" placeholder="Rua">
+        <input type="text" name="numero" placeholder="Número">
+        <input type="text" name="complemento" placeholder="Complemento (se houver)">
+    </div>
+    <div class="endereco-group">
+        <input type="text" name="bairro" placeholder="Bairro">
+        <input type="text" name="cidade" placeholder="Cidade">
+        <input type="text" name="estado" placeholder="Estado">
+    </div>
+    <label>Foto do Imovel:</label>
+    <input type="file" name="imgImovel"><br>
+    <input type="submit" value="Cadastrar Imovel">
+
+</form>
+</body>
+</html>
